d4d15453fe78e72d3ff1f50246705782818be064
Hanns <hannsforever01@gmail.com>
2022-07-29 03:09:29 +0800

    - convert hard coded user into array - login code tidy up

--- DSTR Assignment/login.cpp
+++ DSTR Assignment/login.cpp
@@ -11,56 +11,67 @@ struct user {
 	int role;
 };
 
-inline void userData(user* user) {
-	user[0].userID = 001;
-	user[0].name = "Remy";
-	user[0].email = "remy@mail.com";
-	user[0].password = "1234";
-	user[0].role = 1;
-
-	user[1].userID = 002;
-	user[1].name = "Hanns";
-	user[1].email = "hanns@mail.com";
-	user[1].password = "1234";
-	user[1].role = 2;
-
-	user[2].userID = 003;
-	user[2].name = "Aya";
-	user[2].email = "aya@mail.com";
-	user[2].password = "1234";
-	user[2].role = 2;
+inline void userData(user* users) {
+	users[0] = { 001,"Remy","remy@mail.com","1234",1 };
+	users[1] = { 002,"Hanns","hanns@mail.com","1234",2 };
+	users[2] = { 003,"Aya","aya@mail.com","1234",2 };
 }
 
-inline int validateUser(user user[]) {
+//struct user users[5]
+//	= { {001,"Remy","remy@mail.com","1234",1},
+//		{002,"Hanns","hanns@mail.com","1234",2},
+//		{003,"Aya","aya@mail.com","1234",2} };
+
+//inline void userData(user* users) {
+//	users[0].userID = 001;
+//	users[0].name = "Remy";
+//	users[0].email = "remy@mail.com";
+//	users[0].password = "1234";
+//	users[0].role = 1;
+//
+//	users[1].userID = 002;
+//	users[1].name = "Hanns";
+//	users[1].email = "hanns@mail.com";
+//	users[1].password = "1234";
+//	users[1].role = 2;
+//
+//	users[2].userID = 003;
+//	users[2].name = "Aya";
+//	users[2].email = "aya@mail.com";
+//	users[2].password = "1234";
+//	users[2].role = 2;
+//}
+
+inline int validateUser(user users[]) {
 	char next = 'n';
 	do {
-		string emailAddress;
-		string pass;
 		bool flag = true;
+		string emailAddress, pass;
 		cout << "Enter email: ";
 		cin >> emailAddress;
-
 		cout << "Enter password: ";
 		cin >> pass;
 
-		for (int i = 0; i < 3; ++i)
+		for (int i = 0; i < 5; ++i)
 		{
-			if (user[i].email == emailAddress && user[i].password == pass)
+			if (users[i].email == emailAddress && users[i].password == pass)
 			{
-				cout << "Login successfull. Welcome " << user[i].name;
+				cout << "Login successfull. Welcome " << users[i].name;
 				flag = false;
 				next = 'n';
-				return user[i].role;
+				return users[i].role;
 			}
 		}
 
 		if (flag) {
 			cout << "Login unsuccessfull." << endl;
 			do {
-				cout << "Do you want to continue? (y/n)." << endl;
+				cout << "Try again? (y/n)." << endl;
 				cin >> next;
 			} while (next != 'y' && next != 'n');
+			
 		}
+
 	} while (next == 'y');
 	exit(0);
 }
